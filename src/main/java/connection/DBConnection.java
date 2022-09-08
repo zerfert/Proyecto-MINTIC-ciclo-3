c3cc1195d41778bcdcb2d736165537f3b4af8e69
Brayan-Mahecha <b.1927@hotmail.com>
2022-09-07 22:15:41 -0500

    prueba commit

--- src/main/java/connection/DBConnection.java
+++ src/main/java/connection/DBConnection.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package connection;
 
-/**
- *
- * @author PC
- */
+import java.sql.Connection;
+
+
 public class DBConnection {
+      Connection connection;
+    static String bd = "AppVuelos";
+    static String port = "3306";
+    static String login = "root";
+    static String password = "admin";
     
 }
